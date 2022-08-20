c48648dc8bb3adf74525ae3d88d0d46519958f35
Benjamin <git@github.com>
2022-08-20 09:54:28 +0200

    better logics

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,58 +1,57 @@
-const botResult = document.querySelector('.bot-result');
-const playerResult = document.querySelector('.player-result');
-const score = document.querySelector('.scoreG');
-const msg = document.querySelector('.msg');
-var choices = document.querySelectorAll('.player-choice-img');
-var botValue = 0;
-var playerChoice = '';
-var playerScore = 0;
-var botScore = 0;
+const botResult = document.querySelector('.bot-result'), playerResult = document.querySelector('.player-result');
+const score = document.querySelector('.scoreG'), msg = document.querySelector('.msg');
+const playerChoices = document.querySelectorAll('.player-choice-img'), botChoices = document.querySelectorAll('.bot-choice-img');
+var botValue = 0, playerScore = 0, botScore = 0, result = '';
 
-document.addEventListener('click', e =>{
-    if (e.target.className == 'player-choice-img'){
-        playerChoice = e.target.getAttribute('alt');
-        Play(playerChoice);
-        e.target.style.backgroundColor = 'green';
+let DrawImage = (botChoice, playerChoice) => {
+    botResult.innerHTML = `<img class="bot-choice-img" src="assets/img/${botChoice}.png" alt="${botChoice}">`
+    playerResult.innerHTML = `<img class="player-choice-img" src="assets/img/${playerChoice}.png" alt="${playerChoice}">`
+}
+
+let PrintResult = result => {
+    msg.removeAttribute('class')
+    if (result == 'draw') {
+        msg.classList.add(result)
+        msg.innerHTML = 'égalité !';
+    } else if (result == 'win') {
+        msg.classList.add(result)
+        msg.innerHTML = 'Vous avez gagné !';
+        playerScore++;
+    } else {
+        msg.classList.add(result)
+        msg.innerHTML = 'Vous avez perdu !';
+        botScore++;
+    }
+    score.innerHTML = `${playerScore} : ${botScore}`;
+}
+
+let CheckRules = (botChoice, playerChoice) => {
+    if (botChoice == playerChoice) {
+        result = 'draw';
+    } else if (botChoice == playerChoice - 1 || botChoice == playerChoice+2) {
+        result = 'win';
+    } else {
+        result = 'loose';
+    }
+    PrintResult(result)
+}   
+
+document.addEventListener('click', e => {
+    if (e.target.classList.contains('player-choice-img')) {
+        Play(e.target);
     }
 })
 
-let DrawImage = (value, arg) =>{
-        let image = '';
-        if (value == 0) {
-            image = 'pierre';
-        } else if (value == 1) {
-            image = 'feuille';
-        } else {
-            image = 'ciseaux';
-        }
-        botResult.innerHTML = `<img class="bot-choice-img" src="assets/img/${image}.png" alt="${image}">`
-        playerResult.innerHTML = `<img class="player-choice-img" src="assets/img/${arg}.png" alt="${arg}">`
-}
-
-let Play = arg => {
+let Play = target => {
     botValue = Math.floor(Math.random() * 3);
-    DrawImage(botValue, arg);
-    CheckRules(botValue, arg);
-
-    for (let i = 0; i < choices.length; i++) {
-        choices[i].style.backgroundColor = 'transparent';
+    for (let i = 0; i < playerChoices.length; i++) {
+        playerChoices[i].classList.remove('selected');
+        botChoices[i].classList.remove('selected');
+        if (target == playerChoices[i]) {
+            target.classList.add('selected');
+            CheckRules(botValue, i);
+            DrawImage(botChoices[botValue].getAttribute('alt'), target.getAttribute('alt'));
+        }
     }
-
-    
+    botChoices[botValue].classList.add('selected');
 }
-
-let CheckRules = (arg, arg1) =>{
-    if (arg == 0 && arg1 == 'pierre' || arg == 1 && arg1 == 'feuille' || arg == 2 && arg1 == 'ciseaux') {
-        msg.innerHTML = 'égalité !';
-        msg.style.backgroundColor = 'yellow';
-    } else if (arg == 0 && arg1 == 'feuille' || arg == 1 && arg1 == 'ciseaux' || arg == 2 && arg1 == 'pierre') {
-        msg.innerHTML = 'Vous avez gagné !';
-        msg.style.backgroundColor = 'green';
-        playerScore += 1;
-    } else {
-        msg.innerHTML = 'Vous avez perdu !';
-        msg.style.backgroundColor = 'red';
-        botScore += 1;
-    }
-    score.innerHTML = `${playerScore} : ${botScore}`;
-}   
